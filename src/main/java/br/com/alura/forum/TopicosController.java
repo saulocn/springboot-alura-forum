c1009bcd37e0e63eb99f08a14c6747cbf80b6ac7
Saulo de Carvalho Neto <saulocn@gmail.com>
2019-06-21 14:45:51 -0300

    - Implementação de Dto do Tópico; - Utilização do sprig devtools;

--- src/main/java/br/com/alura/forum/TopicosController.java
+++ src/main/java/br/com/alura/forum/TopicosController.java
@@ -3,19 +3,19 @@ package br.com.alura.forum;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
+
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
-		Topico topico = new Topico("Dúvida 1", "Dúvida de Spring", new Curso("Spring", "Programação"));
-		return Arrays.asList(topico, topico, topico);
+	public List<TopicoDto> lista() {
+		Topico topico = new Topico("Dúvida 3", "Dúvida de Spring", new Curso("SpringBoot", "Programação"));
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 }
