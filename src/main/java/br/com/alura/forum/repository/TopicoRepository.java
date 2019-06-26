4526853a1ad35e5b3800e67c29ece4e71e2473bf
saulo <saulo@saulo-ultra>
2019-06-26 20:32:46 -0300

    - Paginação dos tópicos;

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -2,6 +2,8 @@ package br.com.alura.forum.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,5 +20,8 @@ public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
 	@Query("SELECT t from Topico t where t.curso.nome like :nomeCurso")
 	List<Topico> carregaPorNomeCurso(@Param("nomeCurso") String nomeCurso);
+
+	@Query("SELECT t from Topico t where t.curso.nome like :nomeCurso")
+	Page<Topico> carregaPorNomeCurso(@Param("nomeCurso") String nomeCurso, Pageable paginacao);
 
 }
